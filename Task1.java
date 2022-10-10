7773e469aed8f56e648c94a097cf560aeafc3c1f
Hassan8017 <100116420+Hassan8017@users.noreply.github.com>
2022-10-10 15:36:16 +0500

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.task1;
+
+/**
+ *
+ * @author bsse02183031
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
